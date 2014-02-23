f33c9dc276653a30840b7b01cba94d609106517e
ATX Floods <steven.lyons@austintexas.gov>
2014-02-23 15:37:48 -0600

    Initial Commit

---
+++ Tempo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tempo
+//
+//  Created by Steven Lyons on 2/23/14.
+//  Copyright (c) 2014 Lyonspaw Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
